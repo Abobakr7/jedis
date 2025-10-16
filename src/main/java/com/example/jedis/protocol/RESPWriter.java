e9fe4edca3c30881f5a03befa8a4fadf2e8f4459
Abobakr Osman <mabobakr79@yahoo.com>
2025-10-16 19:19:49 +0300

    feat: add RESP protocol writer

---
+++ src/main/java/com/example/jedis/protocol/RESPWriter.java
@@ -0,0 +1,33 @@
+package com.example.jedis.protocol;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class RESPWriter {
+    private RESPWriter() {}
+
+    public static void writeBulkString(OutputStream out, String msg) throws IOException {
+        out.write(String.format("$%d\r\n%s\r\n", msg.length(), msg).getBytes());
+        out.flush();
+    }
+
+    public static void writeNullBulkString(OutputStream out) throws IOException {
+        out.write("$-1\r\n".getBytes());
+        out.flush();
+    }
+
+    public static void writeInteger(OutputStream out, long num) throws IOException {
+        out.write(String.format(":%d\r\n", num).getBytes());
+        out.flush();
+    }
+
+    public static void writeSimpleString(OutputStream out, String msg) throws IOException {
+        out.write(String.format("+%s\r\n", msg).getBytes());
+        out.flush();
+    }
+
+    public static void writeError(OutputStream out, String msg) throws IOException {
+        out.write(String.format("-ERR %s\r\n", msg).getBytes());
+        out.flush();
+    }
+}
