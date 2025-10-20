fde7e9bdc4c247d2f3fba38ac36ed0a3734fa2e2
Abobakr Osman <mabobakr79@yahoo.com>
2025-10-20 18:00:57 +0300

    feat: support cli args

--- src/main/java/com/example/jedis/App.java
+++ src/main/java/com/example/jedis/App.java
@@ -8,10 +8,12 @@ import com.example.jedis.command.CommandRegistry;
 import com.example.jedis.handler.ClientHandler;
 
 public class App {
-    private static final int PORT = 6379;
+    private static int PORT = 6379;
     private static final CommandRegistry commandRegistry = new CommandRegistry();
 
     public static void main(String[] args) {
+        parseArguments(args);
+        
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             // SO_REUSEADDR ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
@@ -28,5 +30,31 @@ public class App {
             System.out.println("IOException: " + e.getMessage());
             System.exit(-1);
         }
+    }
+
+    public static void parseArguments(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "--port":
+                    if (i + 1 >= args.length) {
+                        exitWithError("--port requires port number");
+                    }
+                    try {
+                        PORT = Integer.parseInt(args[i + 1]);
+                        i++;
+                    } catch (NumberFormatException e) {
+                        exitWithError("Invalid port number '" + args[i + 1] + "'");
+                    }
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void exitWithError(String message) {
+        System.err.println("Error: " + message);
+        System.exit(1);
     }
 }
